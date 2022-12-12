1f88fc3c87ba7cf597bcb3d4cb918027b71b2779
Jesse Driskill <jesse.driskill@gmail.com>
2022-12-12 12:29:41 -0800

    renamed variables and template file; switched "%%" back to "%" in v_cloud-init.template

--- bigip/az_cli/mk_instance.bash
+++ bigip/az_cli/mk_instance.bash
@@ -6,10 +6,10 @@
 # Source AODS variables
 
 # define instance-specific variables here to allow for overriding defaults from vars.bash
-aods_vars="aods-vars.txt"
-aods_vm_base="aods-vm-template.txt"
-instance_vars="bigip01.txt"
-ci_template="cloud-init.template"
+aods_vars="v_aods.txt"
+aods_vm_base="v_aods-vm-template.txt"
+instance_vars="v_bigip01.txt"
+ci_template="v_cloud-init.template"
 cloud_init_def=""
 DEBUG=1
 
@@ -24,14 +24,14 @@ source $instance_vars
 mk_cloud_init() {
   cloud_init_b64=$(perl -pe 's;(\\*)(\$\{([a-zA-Z_][a-zA-Z_0-9]*)\})?;substr($1,0,int(length($1)/2)).($2&&length($1)%2?$2:$ENV{$3||$4});eg' $ci_template | base64 -w0)
 
-  [[ $DEBUG ]] && { echo -n $cloud_init_b64 | base64 -d > ci_data.bash; }
+  [[ $DEBUG ]] && { echo -n $cloud_init_b64 | base64 -d > working/ci_data.bash; }
 }
 
 # Create the VM parameters
 mk_vm_parameters() {
   source $aods_vm_base
 
-  [[ $DEBUG ]] && { echo -n $vm_parameters > vm_params.json; }
+  [[ $DEBUG ]] && { echo -n $vm_parameters > working/vm_params.json; }
 
 }
 
@@ -48,10 +48,6 @@ mk_cloud_init
 # This uses the output of 'mk_cloud_init' and must come second
 mk_vm_parameters 
 
-
-#printf "$cloud_init_def" > 1.bash
-#printf "$cloud_init_b64" > 1.b64
-printf "$vm_parameters" > vm_params.json
 
 #echo "az networkcloud virtualmachine create --name $vmName --resource-group $myrg --subscription $mysub --virtual-machine-parameters '$vm_parameters' --debug"
 
